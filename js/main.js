11c2722bde568e9f339f82c0feb63fc7407f28c6
koany <koroliev@yandex.ru>
2023-04-07 22:49:20 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('input[type="number"]').val('');
+
+// Расчёт скорости
+$('#calculate_speed').click(function (){
+    $('#result_speed').text('Результат: '
+        + (Number($('#distance').val()) / Number($('#hours').val())).toFixed(2) + ' км/ч')})
+
+// Расчёт шоколадок
+$('#calculate_chocolates').click(function (){
+    $('#result_chocolates').text('Шоколадок ' +
+        Math.floor((Number($('#money').val()) / Number($('#price').val()))) + ', сдача ' +
+        (Number($('#money').val()) % Number($('#price').val())))
+})
+
+// Расчёт валют
+let DollarToEuro = 0.92
+$('#calculate_euro').click(function (){
+    $('#result_euro').text(Number($('#dollars').val())*DollarToEuro)
+})
+
+
